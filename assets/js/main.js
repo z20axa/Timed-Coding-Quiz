abd03de25cc48fb2d5eb1e0b4ddc3c4f365fb9f7
z20axa <alejandrospan@hotmail.com>
2022-10-28 16:25:33 -0400

    Adds quiz timer to the JS code

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,52 +15,84 @@
 // -------------------------------------------------------------------------------------------------------------------------------------------
 
 // Collection of objects that hold the questions and answers
-var myQuestions = [
+var quizQuestions = [
   { // array [0]
-    Question1:"What day is today?",
-    answer1:"Monday",
-    answer2:"Thursday",
-    correctAnswer:"Thursday"
+    Question1:"Commonly used data types DO NOT include: ",
+    answer1:"Strings",
+    answer2:"Booleans",
+    answer3: "Alerts",
+    answer4: "Numbers",
+    correctAnswer:"Alerts"
   },
   { // array [1]
-    Question2:"What day is yesterday?",
-    answer1:"Monday",
-    answer2:"Wednesday",
-    correctAnswer:"Wednesday"
+    Question2:"The condition in an if/else statment is enclosed with: ",
+    answer1:"Parenthesis",
+    answer2:"Curly Brackets",
+    answer3: "Quotes",
+    answer4: "Square Brackets",
+    correctAnswer:"Parenthesis"
   },
   { // array [2]
-    Question3:"What day is yesterday?",
-    answer1:"Monday",
-    answer2:"Wednesday",
-    correctAnswer:"Wednesday"
+    Question3:"Arrays in JavaScript can be used to store: ",
+    answer1:"Numbers & Strings",
+    answer2:"Other Arrays",
+    answer3: "Booleans",
+    answer4: "All Of The Above",
+    correctAnswer:"All Of The Above"
   },
   { // array [3]
-    Question4:"What day is yesterday?",
-    answer1:"Monday",
-    answer2:"Wednesday",
-    correctAnswer:"Wednesday"
+    Question4:"String values must be enclosed within _____ when being assigned to variables",
+    answer1:"Commas",
+    answer2:"Quotes",
+    answer3: "Curly Brackets",
+    answer4: "Parenthesis",
+    correctAnswer:"Quotes"
   },
   { // array [4]
-    Question5:"What day is yesterday?",
-    answer1:"Monday",
-    answer2:"Wednesday",
-    correctAnswer:"Wednesday"
+    Question5:"A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer1:"JavaScript",
+    answer2:"Terminal/bash",
+    answer3: "For Loops",
+    answer4: "Console.log",
+    correctAnswer:"Console.log"
   }
-]
+];
 
 var currentQuestion = 0; // to keep a count of the question that is displayed
-var score = 0; // to keep track of score
-var time = 10; // to keep the quiz time
+var currentScore = 0; // to keep track of score
+var countdownTime = 10; // to keep the quiz time
 
 
-var answers = document.querySelector("#answers");
+var quizTimerEl = document.getElementById('#quizTimer');
+var startquizbuttonEl = document.getElementById('#startquizButton');
+
 var questionEl = document.querySelector("#question");
-var startPageMessage = document.querySelector("#startPageMessage");
+var answersEl = document.querySelector("#answers");
 
+// start quiz time function
+function startTimer(countdownTime){
+    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      console.log(countdownTime);
+      // subtract 1 second
+      countdownTime--;
+      quizTimerEl.textContent = countdownTime;
+  
+      if(countdownTime === 0){
+        // stop timer
+        clearInterval(timeInterval);
+  
+        // call the displayMessage function
+        // displayMessage();
+      }
+      
+    }, 1000);
+  };
 
-// function startTimer(){
+startquizbuttonEl.addEventListener("click", function(event){
+  startTimer();
+});
 
-// }
 
 // function addQuestion(){
 //   // create the question elements
@@ -97,331 +129,60 @@ var startPageMessage = document.querySelector("#startPageMessage");
 
 // eventListener here // when i click the start button
 
-function countdown(timer=5) {
-  // var timeLeft = 5;
-
-  // `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function () {
-    console.log(timer);
-    // subtract 1 second
-    timer--;
-    timerEl.textContent = timer;
-
-    if(timer === 0){
-      // stop timer
-      clearInterval(timeInterval);
-
-      return timer;
-      // call the displayMessage function
-      // displayMessage();
-    }
-   
-  }, 1000);
-}
-
-console.log(countdown(10));
-
-// function winGame() {
-//   wordBlank.textContent = "YOU WON!!!🏆 ";
-//   winCounter++
-//   startButton.disabled = false;
-//   setWins()
-// }
-
-// // The loseGame function is called when timer reaches 0
-// function loseGame() {
-//   wordBlank.textContent = "GAME OVER";
-//   loseCounter++
-//   startButton.disabled = false;
-//   setLosses()
-
-// // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-// function startTimer() {
-//   // Sets timer
-//   timer = setInterval(function() {
-//     timerCount--;
-//     timerElement.textContent = timerCount;
-//     if (timerCount >= 0) {
-//       // Tests if win condition is met
-//       if (isWin && timerCount > 0) {
-//         // Clears interval and stops timer
-//         clearInterval(timer);
-//         winGame();
-//       }
-//     }
-//     // Tests if time has run out
-//     if (timerCount === 0) {
-//       // Clears interval
-//       clearInterval(timer);
-//       loseGame();
-//     }
-//   }, 1000);
-// }
-
 // -------------------------------------------------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------------------------------------------------
-
-/**
- * Renders start quiz
- */
-function startQuiz(){
-
-  // add the start button
-  // create
-  var item = document.createElement("li");
-  var btn = document.createElement("button");
-  
-  // modify/add attributes/values
-  btn.textContent = "START";
-  btn.setAttribute("id", "start-button");
-
-  // append to a visible element on the page
-  answers.appendChild(item);
-  item.appendChild(btn);
-
-  // add the question
-  // modify/add attributes/values
-  questionEl.textContent = "Start answering the questions!";
-
-  var timeLeft = 10;
-
-  var timerEl = document.createElement("span");
-  var startCountdown = countdown(timeLeft);
-  if(startCountdown !== timeLeft) {
-    console.log(startCountdown, timeLeft);
-    timerEl.textContent = timeLeft;
-    questionEl.appendChild(timerEl)
-  }
-}
-
-function addQuestions(){
-  // clear start page display
-  startPageMessage.innerHTML = "";
-  
-  // q: "Question 1?",
-  // id: 1,  
-  var question = questions[currentQuestion];
-
-  questionEl.textContent = question.q;
-
-  // clear answers
-  answers.innerHTML = '';
-
-
-  for(var i = 0; i < question.answers.length; i++){
-    console.log(question.answers[i]);
-
-    // <li>
-    //       <button class="option-button" data-index="2" data-value="5" data-next="-1">Rate 5</button>
-    //     </li>
-    var item = document.createElement('li'); // create
-    var button = document.createElement('button');
-    // modify
-    button.setAttribute("class", "option-button");
-    button.textContent = question.answers[i];
-    button.dataset.index = currentQuestion;
-    button.dataset.next = currentQuestion + 1;
-   
-    if(question.answers[i] === question.correct){
-      button.dataset.value = "correct";
-    }
-    else{
-      button.dataset.value = "incorrect";
-    }
-    
-    answers.appendChild(item);
-    item.appendChild(button); // append
-  }
-}
-
-// call startQuiz to show the first screen
-startQuiz();
-
-answers.addEventListener("click", function(event){
- 
-  if(child.matches("#start-button")){
-    addQuestions();
-  }
-  if(child.matches(".option-button")){
-    if(child.dataset.next < questions.length){
-      currentQuestion = parseInt(child.dataset.next);
-      if(child.dataset.value === "correct"){
-        ans++;
-      }
-      addQuestions();
-    }
-    else{
-      if(child.dataset.value === "correct"){
-        ans++;
-      }
-
-      // save the ans to localstorage
-      question.textContent = "Yay you got ... " + ans + " questions!";
-      
-      answers.innerHTML = "";
-    }
-  }
-})
-
-// -------------------------------------------------------------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------------------------------------------------------------
-
-// var emailInput = document.querySelector("#email");
-// var passwordInput = document.querySelector("#password");
-// var signUpButton = document.querySelector("#sign-up");
-// var msgDiv = document.querySelector("#msg");
-// var userEmailSpan = document.querySelector("#user-email");
-// var userPasswordSpan = document.querySelector("#user-password");
-
-// renderLastRegistered();
 
 // /**
-//  * Sets the class and message
-//  * @param {*} type - class 
-//  * @param {*} message - text
+//  * Renders start quiz
 //  */
-// function displayMessage(type, message) {
-//   msgDiv.textContent = message;
-//   // msgDiv.textContent = "banana";
-//   // msgDiv.setAttribute("class", type);
-//   msgDiv.setAttribute("");
-// }
-
-// /**
-//  * render - process the information so we can see it
-//  * because it is declared, the definition of the function will be the hoisted to the top
-//  */
-// function renderLastRegistered() {
-//   // TODO: Retrieve the last email and password and render it to the page
-//   var email = localStorage.getItem("email");
-//   var password = localStorage.getItem("password");
-
-//   userEmailSpan.textContent = email;
-//   userPasswordSpan.textContent = password;
-// }
-
-// signUpButton.addEventListener("click", function(event) {
-//   event.preventDefault();
-
-//   var email = document.querySelector("#email").value;
-//   var password = document.querySelector("#password").value;
-
-//   if (email === "") {
-//     displayMessage("error", "Email cannot be blank");
-//   } else if (password === "") {
-//     displayMessage("error", "Password cannot be blank");
-//   } else {
-//     displayMessage("success", "Registered successfully");
-
-//   // TODO: Save email and password to localStorage and render the last registered user
-//   localStorage.setItem("email", email);
-//   localStorage.setItem("password", password);
-//   // call the render function that we defined before 
-//   // - basically repeating those lines as if they were written here
-//   renderLastRegistered();
-//   // var email = localStorage.getItem("email");
-//   // var password = localStorage.getItem("password");
-
-//   // userEmailSpan.textContent = email;
-//   // userPasswordSpan.textContent = password;
-//   }
-// });
-
-// -------------------------------------------------------------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------------------------------------------------------------
-
-
-// // the kitten app starts here
-// // collection of kitten objects
-// var kittens = [
-//   {
-//     q: "How do you rate this kitten?",
-//     id: 1,  
-//     image: "assets/images/kitten1.jfif",
-//     rating: ["Rate 5", "Rate 4", "Don't take this kitten!"],
-//     incorrect: "Don't take this kitten!"
-//   },
-//   {
-//     q: "How do you rate this kitten?",
-//     id: 2,  
-//     image: "assets/images/kitten2.jfif",
-//     rating: ["Rate 5", "Rate 4", "Don't take this kitten!"],
-//     incorrect: "Don't take this kitten!"
-//   },
-//   {
-//     q: "How do you rate this kitten?",
-//     id: 3,  
-//     image: "assets/images/kitten3.jfif",
-//     rating: ["Rate 5", "Rate 4", "Don't take this kitten!"],
-//     incorrect: "Don't take this kitten!"
-//   },
-//   {
-//     q: "How do you rate this kitten?",
-//     id: 4,  
-//     image: "assets/images/kitten1.jfif",
-//     rating: ["Rate 5", "Rate 4", "Destroy this kitten!"],
-//     incorrect: "Destroy this kitten!"
-//   }
-// ];
-
-// var currentKitten = 0;
-// var ans = 0;
-// var menu = document.querySelector("#menu");
-// var heading = document.querySelector("#heading");
-// var kittenFigure = document.querySelector("#kitten");
-
-// /**
-//  * Renders start screen
-//  */
-// function startScreen(){
+// function startQuiz(){
 
 //   // add the start button
 //   // create
 //   var item = document.createElement("li");
 //   var btn = document.createElement("button");
+  
 //   // modify/add attributes/values
-//   btn.textContent = "start";
+//   btn.textContent = "START";
 //   btn.setAttribute("id", "start-button");
 
 //   // append to a visible element on the page
-//   menu.appendChild(item);
+//   answers.appendChild(item);
 //   item.appendChild(btn);
 
-//   // add the heading
+//   // add the question
 //   // modify/add attributes/values
-//   heading.textContent = "Start Adding Kittens to your collection!"
+//   questionEl.textContent = "Start answering the questions!";
+
+//   var timeLeft = 10;
+
+//   var timerEl = document.createElement("span");
+//   var startCountdown = countdown(timeLeft);
+//   if(startCountdown !== timeLeft) {
+//     console.log(startCountdown, timeLeft);
+//     timerEl.textContent = timeLeft;
+//     questionEl.appendChild(timerEl)
+//   }
 // }
 
-// function renderKitten(){
-//   // {
-//   //   q: "How do you rate this kitten?",
-//   //   id: 1,  
-//   //   image: "assets/images/kitten1.jfif"
-//   // },
-//   var kitten = kittens[currentKitten];
+// function addQuestions(){
+//   // clear start page display
+//   startPageMessage.innerHTML = "";
+  
+//   // q: "Question 1?",
+//   // id: 1,  
+//   var question = questions[currentQuestion];
 
-//   heading.textContent = kitten.q;
+//   questionEl.textContent = question.q;
 
-//   kittenFigure.innerHTML = '';
+//   // clear answers
+//   answers.innerHTML = '';
 
-//   var kittenImage = document.createElement("img"); //create
 
-//   kittenImage.setAttribute("src", kitten.image) //modify
-//   kittenImage.setAttribute("alt", "Kitten"); // modify
-
-//   kittenFigure.append(kittenImage); // append
-
-//   // clear menu
-//   menu.innerHTML = '';
-
-//   for(var i = 0; i < kitten.rating.length; i++){
-//     console.log(kitten.rating[i]);
+//   for(var i = 0; i < question.answers.length; i++){
+//     console.log(question.answers[i]);
 
 //     // <li>
 //     //       <button class="option-button" data-index="2" data-value="5" data-next="-1">Rate 5</button>
@@ -430,56 +191,47 @@ answers.addEventListener("click", function(event){
 //     var button = document.createElement('button');
 //     // modify
 //     button.setAttribute("class", "option-button");
-//     button.textContent = kitten.rating[i];
-//     button.dataset.index = currentKitten;
-//     button.dataset.next = currentKitten + 1;
-//     // if(kitten.rating[i] !== kitten.incorrect){
-//     //   button.dataset.value = "correct";
-//     // }
-//     // else{
-//     //   button.dataset.value = "incorrect";
-//     // }
-//     if(kitten.rating[i] === kitten.incorrect){
-//       button.dataset.value = "incorrect";
-//     }
-//     else{
+//     button.textContent = question.answers[i];
+//     button.dataset.index = currentQuestion;
+//     button.dataset.next = currentQuestion + 1;
+   
+//     if(question.answers[i] === question.correct){
 //       button.dataset.value = "correct";
 //     }
+//     else{
+//       button.dataset.value = "incorrect";
+//     }
     
-//     menu.appendChild(item);
-//     item.appendChild(button);// append
+//     answers.appendChild(item);
+//     item.appendChild(button); // append
 //   }
 // }
 
-// // call startscreen to show the first screen
-// startScreen();
+// // call startQuiz to show the first screen
+// startQuiz();
 
-// menu.addEventListener("click", function(event){
-//   console.log(ans);
-
-//   var child = event.target;
-//   console.log(child);
-
+// answers.addEventListener("click", function(event){
+ 
 //   if(child.matches("#start-button")){
-//     renderKitten();
+//     addQuestions();
 //   }
 //   if(child.matches(".option-button")){
-//     if(child.dataset.next < kittens.length){
-//       currentKitten = parseInt(child.dataset.next);
+//     if(child.dataset.next < questions.length){
+//       currentQuestion = parseInt(child.dataset.next);
 //       if(child.dataset.value === "correct"){
 //         ans++;
 //       }
-//       renderKitten();
+//       addQuestions();
 //     }
 //     else{
 //       if(child.dataset.value === "correct"){
 //         ans++;
 //       }
 
-//       heading.textContent = "Yay you got ... " + ans + " kittens!";
 //       // save the ans to localstorage
-//       kittenFigure.innerHTML = "";
-//       menu.innerHTML = "";
+//       question.textContent = "Yay you got ... " + ans + " questions!";
+      
+//       answers.innerHTML = "";
 //     }
 //   }
 // })
