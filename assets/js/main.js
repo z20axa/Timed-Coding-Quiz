937c82a3ac539e548c1537768b1f7fae930ca71b
z20axa <alejandrospan@hotmail.com>
2022-10-29 18:28:03 -0400

    Adds comments to the JS code

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,29 +44,24 @@ var quizQuestions = [
 
 var currentQuestion = 0; // variable to keep a count of the question that is displayed
 var currentScore = 0; // variable to keep track of the score
-var countdownTime = 10; // vairable to keep the quiz countodnw time
+var countdownTime = 100; // vairable to keep the quiz countodnw time
 
+// doc variable declarations 
 var quizTimerEl = document.querySelector('#quizTimer');
 var startquizbuttonEl = document.querySelector('#startquizButton');
 var welcomesectionEl = document.querySelector('#welcomeSection');
 var questionsectionEl = document.querySelector('#questionsSection');
 var questionEl = document.querySelector("#question");
 var answersEl = document.querySelector("#answers");
+var answerMessage = document.querySelector('#answerMessage');
 
-// <section style="display:none" id="questionsSection">
-//  <h2 id="question"></h2>
-//  <ol id="answers">
-//    <button></button>
-//  </ol>
-// </section>
-
-// creates list elements
-// Create ordered list items
+// create ordered list elements
 var li1 = document.createElement("li");
 var li2 = document.createElement("li");
 var li3 = document.createElement("li");
 var li4 = document.createElement("li");
 
+// create buttons for each ordered list element created 
 var li1button = document.createElement('button');
 var li2button = document.createElement('button');
 var li3button = document.createElement('button');
@@ -78,50 +73,38 @@ answersEl.appendChild(li2);
 answersEl.appendChild(li3);
 answersEl.appendChild(li4);
 
-// append all the button elements to the appended list elements above to display the question's answers
+// append all the button elements to the appended list elements 
 li1.appendChild(li1button);
 li2.appendChild(li2button);
 li3.appendChild(li3button);
 li4.appendChild(li4button);
 
-
-startquizbuttonEl.addEventListener("click", function(event){
-  startTimer();
-  // welcomesectionEl.innerHTML="";
-
-  // add questions function call
-  addQuestion();
-});
-
-// start quiz time function
+// function declaration to start quiz countown time 
 function startTimer(){
-  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  // `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
     // console.log(countdownTime);
     // subtract 1 second
     countdownTime--;
-    quizTimerEl.textContent = countdownTime;
+    quizTimerEl.textContent = countdownTime; // modify the text/attributes the quiz time element for display
 
-    if(countdownTime === 0){
-      // stop timer
-      clearInterval(timeInterval);
+    if(countdownTime===0){
+      clearInterval(timeInterval); // stop the quiz countdown time
 
       return;
 
       // call function to display alldoneSection
       // displayMessage();
     }
-    
   }, 1000);
 };
 
+// function declaration to add the quiz question and answer choices 
 function addQuestion(){
-  // hides the welcome section display
-  welcomesectionEl.setAttribute("style", "display:none"); 
+  welcomesectionEl.setAttribute("style", "display:none");  // hides the welcome section display
   // welcomesectionEl.innerHTML=""; // another way to hide the welcome section display
 
-  // display the questions and answers section
-  questionsectionEl.setAttribute("style", "display:block"); 
+  questionsectionEl.setAttribute("style", "display:block"); // display the questions and answers section
 
   // modify the text/attributes
   questionEl.textContent=quizQuestions[currentQuestion].Question;
@@ -131,53 +114,64 @@ function addQuestion(){
   li4button.textContent=quizQuestions[currentQuestion].answer4;
 };
 
-// event listener for all the buttons, 
-li1button.addEventListener("click", function(event){
-  checkAnswers(event);
-    
-  // add questions function call
-  addQuestion();
-});
-
-li2button.addEventListener("click", function(event){
-  checkAnswers(event);
-    
-  // add questions function call
-  addQuestion();
-});
-
-li3button.addEventListener("click", function(event){
-  checkAnswers(event);
-    
-  // add questions function call
-  addQuestion();
-});
-
-li4button.addEventListener("click", function(event){
-  checkAnswers(event);
-    
-  // add questions function call
-  addQuestion();
-});
-
+// function declaration to check if the answer is correct or wrong 
 function checkAnswers(event){
-  var child = event.target;
+  var child = event.target; // variable declaration
   // console.log(child);
   // console.log(child.innerText);
   
+  // to check if the answer is correct 
   if(child.innerText===quizQuestions[currentQuestion].correctAnswer){
     console.log("You got it right!");
-    currentScore+=10;
-  }
-  else {
+    // answerMessage.setAttribute("style", "display:block"); 
+    // answerMessage.textContent = "You got it right!";
+    currentScore+=10; // if the answer is correct adds 10 points to the quiz score
+  } else // to check if the answer is wrong 
+  {
     console.log("You got it wrong!")
-    countdownTime-=20;
-    currentScore+=0;   
-  }
+    // answerMessage.setAttribute("style", "display:block"); 
+    // answerMessage.textContent = "You got it wrong!";
+    countdownTime-=10; // time penalty, if the answer is wrong deducts 10 seconds from the quiz countdown time
+    currentScore+=0; // if the answer is wrong no points add it to the quiz score
+  };
   
-  currentQuestion++;
+  currentQuestion++; // question index to go the next questions after checking the previous question answers 
   
   console.log(countdownTime);
   console.log(currentScore);
   };
+
+// start quiz countdown time click event listerner 
+startquizbuttonEl.addEventListener("click", function(event){
+  startTimer(); // function call to start the quiz countdown time
+  
+  addQuestion(); // function call to add the quiz question and answer choices
+});
+
+// event listener for all the questions' answer buttons
+li1button.addEventListener("click", function(event){
+  checkAnswers(event); // function call to check if the answer is correct or wrong 
+    
+  addQuestion(); // function call to add the quiz question and answer choices
+});
+
+li2button.addEventListener("click", function(event){
+  checkAnswers(event); // function call to check if the answer is correct or wrong 
+    
+  addQuestion(); // function call to add the quiz question and answer choices
+});
+
+li3button.addEventListener("click", function(event){
+  checkAnswers(event); // function call to check if the answer is correct or wrong 
+    
+  addQuestion(); // function call to add the quiz question and answer choices
+});
+
+li4button.addEventListener("click", function(event){
+  checkAnswers(event); // function call to check if the answer is correct or wrong 
+    
+  addQuestion(); // function call to add the quiz question and answer choices
+});
+
+
 
